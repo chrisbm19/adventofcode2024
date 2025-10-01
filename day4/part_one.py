f83f7c0093316e3c6f411f53c7f85588c983abf3
chrisbm19 <christopher.mitchell.business@gmail.com>
2025-10-01 00:25:40 +0000

    day4 part_one still in progress

---
+++ day4/part_one.py
@@ -0,0 +1,107 @@
+def probe(board, row, col):
+    found = 0
+    # xmas is spelled left-to-right
+    try:
+        if board[row][col+1] == "M" and board[row][col+2] == "A" and board[row][col+3] == "S":
+            found += 1
+            #board[row][col+1] = "#" 
+            #board[row][col+2] = "#"
+            #board[row][col+3] = "#"
+    except IndexError:
+        print("", end="")
+    # xmas is spelled out right-to-left
+    try:
+        if board[row][col-1] == "M" and board[row][col-2] == "A" and board[row][col-3] == "S":
+            found += 1
+            #board[row][col-1] = "#" 
+            #board[row][col-2] = "#" 
+            #board[row][col-3] = "#"
+    except IndexError:
+        print("", end="")
+    # xmas is spelled out straight down
+    try:
+        if board[row+1][col] == "M" and board[row+2][col] == "A" and board[row+3][col] == "S":
+            found += 1
+            #board[row+1][col] = "#" 
+            #board[row+2][col] = "#" 
+            #board[row+3][col] = "#"
+    except IndexError:
+        print("", end="")
+    # xmas is spelled out straight up
+    try:
+        if board[row-1][col] == "M" and board[row-2][col] == "A" and board[row-3][col] == "S":
+            found += 1
+            #board[row-1][col] = "#" 
+            #board[row-2][col] = "#" 
+            #board[row-3][col] = "#"
+    except IndexError:
+        print("", end="")
+    # xmas is spelled out diagonally to the right (down)
+    try:
+        if board[row+1][col+1] == "M" and board[row+2][col+2] == "A" and board[row+3][col+3] == "S":
+            found += 1
+            #board[row+1][col+1] = "#" 
+            #board[row+2][col+2] = "#" 
+            #board[row+3][col+3] = "#"
+    except IndexError:
+        print("", end="")
+    # xmas is spelled out diagonally to the right (up)
+    try:
+        if board[row-1][col+1] == "M" and board[row-2][col+2] == "A" and board[row-3][col+3] == "S":
+            found += 1
+            #board[row-1][col+1] = "#" 
+            #board[row-2][col+2] = "#" 
+            #board[row-3][col+3] = "#"
+    except IndexError:
+        print("", end="")
+    # xmas is spelled out diagonally to the left (down)
+    try:
+        if board[row+1][col-1] == "M" and board[row+2][col-2] == "A" and board[row+3][col-3] == "S":
+            found += 1
+            #board[row+1][col-1] = "#" 
+            #board[row+2][col-2] = "#" 
+            #board[row+3][col-3] = "#"
+    except IndexError:
+        print("", end="")
+    # xmas is spelled out diagonally to the left (up)
+    try:
+        if board[row-1][col-1] == "M" and board[row-2][col-2] == "A" and board[row-3][col-3] == "S":
+            found += 1
+            #board[row-1][col-1] = "#" 
+            #board[row-2][col-2] = "#" 
+            #board[row-3][col-3] = "#"
+    except IndexError:
+        print("", end="")
+    return found
+
+def search(board, rows, cols):
+    sum = 0
+    for r in range(0, rows):
+        for c in range(0, cols):
+            try:
+                if board[r][c] == "X":
+                    found = probe(board, r, c)
+                    sum += found
+            except IndexError:
+                print("", end="")
+    return sum
+
+def main():
+    board = []
+
+    # get puzzle_input as a 2d list to represent the word search
+    with open("puzzle_input.txt", "r", encoding=None) as file:
+        for line in file:
+            split_line = list(line)
+            board.append(split_line)
+
+    rows = len(board)
+    cols = len(board[0])
+
+    sum = search(board, rows, cols)
+
+    print(f"Sum: {sum}")
+    #print(board)
+    return
+
+main()
